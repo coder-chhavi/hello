e9a2044f806001b749aabd07e67e4b2cafb8917d
unknown <coderchhavi1>
2025-07-19 22:51:54 +0530

    roll your dice

--- DiceRoll.py
+++ DiceRoll.py
@@ -2,7 +2,7 @@ import random
 print('Welcome guys!! let us play a dice game')
 print('1.Roll the dice')
 option=int(input('enter option: '))
-dice=random.randint(1,7)
+dice=random.randint(1,6)
 while True:
     if option ==1:
         if dice==6:
